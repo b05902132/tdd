5934ccf1f70f1c4e945380d0340acf1ddb386fcd
b05902132 <b05902132@csie.ntu.edu.tw>
2021-01-31 21:12:36 +0800

    Use chrome instead of firefox to make FT faster

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,7 +8,7 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
@@ -82,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies, etc.
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
         # Francis visits the home page. There's no sign of Edith's list.
 
