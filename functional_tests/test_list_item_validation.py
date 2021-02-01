9ee5f9e4f05cb73053bc6be28f9a743abee3f510
b05902132 <b05902132@csie.ntu.edu.tw>
2021-02-02 01:41:54 +0800

    refactor find error element in FT.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from unittest import skip
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_item(self):
         # Edith goes to the home page and accidently tries to submit
         # an empty list item. She hits Enter on the empty input box.
@@ -49,7 +52,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message.
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
 
@@ -63,7 +66,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
 
         # She starts typing in the input box to clear the error
@@ -71,6 +74,6 @@ class ItemValidationTest(FunctionalTest):
 
         # She is pleased to see that the error message disappears
         self.wait_for(lambda: self.assertFalse(
-            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+            self.get_error_element().is_displayed()
         ))
 
