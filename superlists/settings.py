d43fbf1760a92928f530829657315806076b507f
b05902132 <b05902132@csie.ntu.edu.tw>
2021-02-02 17:53:36 +0800

    Add models for accounts.

--- superlists/settings.py
+++ superlists/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
