bbadbb7003f39d290a273d6bb2c1b270d9bc99a1
b05902132 <b05902132@csie.ntu.edu.tw>
2021-02-07 02:37:46 +0800

    Add server side session management for remote FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import WebDriverException
 import os
 import time
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -12,6 +14,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
